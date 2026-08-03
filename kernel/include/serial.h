449b6e4a3d6a739e1838346c9ed3b5346ebe994f
cibocaz <cibocaz@tenon.dev>
2026-08-03 17:47:30 +0800

    feat: COM1串口输出kprint/kprintln

--- kernel/include/serial.h
+++ kernel/include/serial.h
@@ -1,6 +1,16 @@
 #ifndef KERNEL_SERIAL_H
 #define KERNEL_SERIAL_H
 
+/**
+ * 向QEMU串口输出字符串
+ * @param chars[] 字符串
+ */
 void kprint(char chars[]);
+
+/**
+ * 向QEMU串口输出字符串，自动换行
+ * @param chars[] 字符串
+ */
+void kprintln(char chars[]);
 
 #endif
--- kernel/serial/serial.c
+++ kernel/serial/serial.c
@@ -1,6 +1,24 @@
 #include "serial.h"
+#include "asm.h"
+
+#define COM1 0x3F8
+
+static int is_transmit_empty(void)
+{
+    return asm_inb(COM1 + 5) & 0x20;
+}
 
 void kprint(char chars[])
 {
-    (void)chars;
+    for (int i = 0; chars[i] != '\0'; i++)
+    {
+        while (!is_transmit_empty()) {}
+        asm_outb(COM1, chars[i]);
+    }
+}
+
+void kprintln(char chars[])
+{
+    kprint(chars);
+    kprint("\n");
 }
