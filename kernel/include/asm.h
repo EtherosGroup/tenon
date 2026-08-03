f02e28bab7bc07423d7ee6171d5cd1c0a66e6757
IsCibocaz <iscibocaz@outlook.com>
2026-08-03 18:45:24 +0800

    chore: format asm.h

--- kernel/include/asm.h
+++ kernel/include/asm.h
@@ -5,30 +5,33 @@
 
 /* I/O Port Operations (x86 only) */
 ku8  asm_inb(ku16 port);
-void     asm_outb(ku16 port, ku8 value);
+void asm_outb(ku16 port, ku8 value);
 ku16 asm_inw(ku16 port);
-void     asm_outw(ku16 port, ku16 value);
+void asm_outw(ku16 port, ku16 value);
 ku32 asm_inl(ku16 port);
-void     asm_outl(ku16 port, ku32 value);
-void     asm_io_wait(void);
+void asm_outl(ku16 port, ku32 value);
+void asm_io_wait(void);
 
 /* MSR Operations (x86 only) */
 ku64 asm_rdmsr(ku32 msr);
-void     asm_wrmsr(ku32 msr, ku64 value);
+void asm_wrmsr(ku32 msr, ku64 value);
 
 /* Control Register Operations */
 unsigned long asm_read_cr0(void);
 unsigned long asm_read_cr2(void);
 unsigned long asm_read_cr3(void);
 unsigned long asm_read_cr4(void);
-void          asm_write_cr0(unsigned long value);
-void          asm_write_cr3(unsigned long value);
-void          asm_write_cr4(unsigned long value);
+void asm_write_cr0(unsigned long value);
+void asm_write_cr3(unsigned long value);
+void asm_write_cr4(unsigned long value);
 
 /* CPUID */
-void asm_cpuid(ku32 leaf, ku32 subleaf,
-               ku32 *eax, ku32 *ebx,
-               ku32 *ecx, ku32 *edx);
+void asm_cpuid
+(
+    ku32 leaf, ku32 subleaf,
+    ku32 *eax, ku32 *ebx,
+    ku32 *ecx, ku32 *edx
+);
 
 /* Instruction Wrappers */
 void asm_hlt(void);
@@ -42,7 +45,7 @@ void asm_invlpg(void *addr);
 
 /* RFLAGS/EFLAGS */
 unsigned long asm_read_rflags(void);
-void          asm_write_rflags(unsigned long flags);
+void asm_write_rflags(unsigned long flags);
 
 /* Timestamp Counter */
 ku64 asm_rdtsc(void);
@@ -64,4 +67,4 @@ ku16 asm_read_fs(void);
 ku16 asm_read_gs(void);
 ku16 asm_read_ss(void);
 
-#endif /* KERNEL_ASM_H */
+#endif
